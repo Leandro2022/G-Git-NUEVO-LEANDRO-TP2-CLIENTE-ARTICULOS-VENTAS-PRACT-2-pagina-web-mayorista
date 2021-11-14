82118e574c344d967530065e89e411498c088b88
Leandro2022 <91895023+Leandro2022@users.noreply.github.com>
2021-11-14 12:32:56 -0300

    Add files via upload
    Sistema venta mayorista.. es una pagina web donde veremos sus ventana de un sistema y sus pasos a seguir...

---
+++ pagina_web2/Bienvenido.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+            <p>Hola tus datos fueron enviados a al Correo de la Empresa</p>
+               <p> Su nombre es <?php echo $_POST["NOMBRE"]?> </p>
+               <p> Su Apellido es <?php echo $_POST["APELLIDO"]?> </p>
+               <p> Su DNI es <?php  echo $_POST ["DNI"]?> </p>
+               <p> Su telefono es <?php echo $_POST ["TELEFONO"] ?> </p>
+               <p> Su E_mail es <?php echo $_POST ["E-MAIL"] ?> </p>
+               <p> Su Dirección es <?php echo $_POST ["DIRECCION"] ?> </p>
+               <p> Su localidad es <?php echo $_POST ["LOCALIDAD"] ?> </p>
+    </body>
+</html>
